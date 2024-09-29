9331c7a5a0b5f9efcb46776086b960bf5de0c7e4
Ayush Dharaskar <118068830+Ayush-Dharaskar@users.noreply.github.com>
2024-09-29 18:23:04 +0530

    Add files via upload

--- IS/lab3/rsa_manual.py
+++ IS/lab3/rsa_manual.py
@@ -16,7 +16,7 @@ def generate_rsa_keys(bits=2048):
     # Step 1: Generate two distinct prime numbers
     p = getPrime(bits // 2)
     q = getPrime(bits // 2)
-
+    print(f"value of p:{p}, \nvalue of q:{q}")
     # Step 2: Calculate n
     n = p * q
 
@@ -25,6 +25,7 @@ def generate_rsa_keys(bits=2048):
 
     # Step 4: Choose public exponent e
     e = generate_random_e(phi_n) # Common choice for e
+    print(f"value of e:{e}")
 
    # Step 5: Calculate private exponent d
     d = inverse(e, phi_n)
@@ -55,10 +56,10 @@ def rsa_decrypt(private_key, ciphertext):
 # Main code
 if __name__ == "__main__":
     # Message to be encrypted
-    message = b'Asymmetric Encryption'
+    message = b'Asymmetric'
 
     # Generate RSA public and private keys
-    public_key, private_key = generate_rsa_keys(bits=2048)
+    public_key, private_key = generate_rsa_keys(bits=80)
 
     # Encrypt the message with the public key
     ciphertext = rsa_encrypt(public_key, message)
@@ -68,60 +69,3 @@ if __name__ == "__main__":
     decrypted_message = rsa_decrypt(private_key, ciphertext)
     print("Decrypted message:", decrypted_message.decode())
 
-# from sympy import mod_inverse
-#
-# # RSA parameters (using larger primes for practicality)
-# p = 61
-# q = 53
-# n = p * q
-# phi_n = (p - 1) * (q - 1)
-#
-#
-# def gcd(p, q):
-#     # Use Euclid's algorithm to find the GCD.
-#     while q != 0:
-#         p, q = q, p % q
-#     return p
-#
-#
-# def coprime(nm):
-#     for i in range(2, nm):
-#         if gcd(i, nm) == 1:
-#             return i
-#     return None
-#
-#
-# e = coprime(phi_n)
-# # Compute private key d
-# d = mod_inverse(e, phi_n)
-# print(d)
-#
-#
-# # RSA encryption function
-# def rsa_encrypt(message, n, e):
-#     encmsg = []
-#     for char in message:
-#         encmsg.append((pow(ord(char), e)) % n)
-#
-#     return encmsg
-#
-#
-# # RSA decryption function
-# def rsa_decrypt(encrypted_int, n, d):
-#     decmsg = ""
-#     for i in encrypted_int:
-#         decmsg += chr(pow(i, d) % n)
-#
-#     return decmsg
-#
-#
-# # Example message
-# message = "Asymmetric Encryption"
-#
-# # Encrypt the message
-# encrypted_message = rsa_encrypt(message, n, e)
-# print("Encrypted message (integer):", encrypted_message)
-#
-# # Decrypt the message
-# decrypted_message = rsa_decrypt(encrypted_message, n, d)
-# print("Decrypted message:", decrypted_message)
